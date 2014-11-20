b3aa5b47e60bf27bc98c3b4527183eca6b331b83
“Lester <lester@applester.co>
2014-11-20 22:45:37 +0800

    added openssl

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -22,71 +22,12 @@ node default {
     enforce_sql => true,
     require     => Class['txtcmdr::postfix'],
   }
+  ->
+  exec{"rm ${txtcmdr::postfix::initdb}":}
 
-
-  notify {"yes - ${txtcmdr::postfix::initdb}":
-    require => Class['txtcmdr::postfix'],
-  }
+  class{'txtcmdr::secure':}
 
 /*
-
-  postfix::map{'mysql-virtual-mailbox-domains.cf':
-    maps => {
-      user => $postfix_user,
-      password => $postfix_pass,
-      hosts => '127.0.0.1',
-      dbname => $postfix_db,
-      query => 'select 1 from virtual_domains where name=\'%s\''
-    },
-  }
-  ->  
-  postfix::postconf{'virtual_mailbox_domains':
-    value => "${postfix::config_dir}/mysql-virtual-mailbox-domains.cf"
-  }
-
-  postfix::map{'mysql-virtual-mailbox-maps.cf':
-    maps => {
-      user => $postfix_user,
-      password => $postfix_pass,
-      hosts => '127.0.0.1',
-      dbname => $postfix_db,
-      query => 'select 1 from virtual_users where email=\'%s\''
-    },
-  }
-  ->
-  postfix::postconf{'virtual_mailbox_maps':
-    value => "${postfix::config_dir}/mysql-virtual-mailbox-maps.cf"
-  }
-
-  postfix::map{'mysql-virtual-alias-maps.cf':
-    maps => {
-      user => $postfix_user,
-      password => $postfix_pass,
-      hosts => '127.0.0.1',
-      dbname => $postfix_db,
-      query => 'select 1 from virtual_aliases where source=\'%s\''
-    },
-  }
-  ->
-  postfix::postconf{'virtual_alias_maps':
-    value => "${postfix::config_dir}/mysql-virtual-alias-maps.cf"
-  }
-
-
-  package{'postfix-mysql':
-    ensure => installed,
-    require => Class['postfix'],
-  }
-
-  group{'vmail': gid => 5000}
- 
-  user{'vmail':
-    ensure     => present,
-    uid        => 5000,
-    gid        => 'vmail',
-    home       => '/var/vmail',
-    managehome => true,
-  }
 
   package{'roundcube':}
 
@@ -96,31 +37,9 @@ node default {
 
   package{'phpmyadmin':}
 
-  openssl::certificate::x509{'mailserver':
-    ensure       => present,
-    country      => 'PH',
-    organization => 'Applester Dev\'t. Corporation',
-    unit         => 'Computing Division',
-    commonname   => $fqdn,
-    base_dir     => '/tmp',
-    days         => 3650,
-  }
 
-  file{'/etc/ssl/private/mailserver.pem':
-    ensure  => present,
-    source  => '/tmp/mailserver.key',
-    require => Openssl::Certificate::X509['mailserver'],
-  }
-  ->
-  exec{'rm /tmp/mailserver.key':}
 
-  file{'/etc/ssl/certs/mailserver.pem':
-    ensure  => present,
-    source  => '/tmp/mailserver.crt',
-    require => Openssl::Certificate::X509['mailserver'],
-  }
-  ->
-  exec{'rm /tmp/mailserver.crt':}
+
 */
 
   Firewall <||>
