e3f4f12227143418a943f2e512a818a70862686d
“Lester <lester@applester.co>
2014-11-24 10:23:57 +0800

    preparing for next module (bind9)

--- puppet/manifests/nodes.pp
+++ puppet/manifests/nodes.pp
@@ -17,44 +17,6 @@ node default {
   
   class{'txtcmdr::dovecot':}
 
-/*
-  package{'roundcube':}
-  package{'roundcube-plugins':
-    require => Package['roundcube'],
-  }
-  package{'phpmyadmin':}
-*/
-
   Firewall <||>
 
-/*  
-  class{'dovecot':
-    config_file_group => 'vmail',
-    config_file_mode  => 'g+r',
-    package    => ['dovecot-imapd','dovecot-pop3d','dovecot-mysql','dovecot-managesieved'],
-    source_dir => 'puppet:///modules/txtcmdr/dovecot',
-    require    => [Class['txtcmdr'],File['/etc/ssl/private/mailserver.pem'],File['/etc/ssl/certs/mailserver.pem']],
-  }
-  
-  file{'/etc/dovecot/dovecot-sql.conf.ext':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => 'go=',    
-    require => Class['dovecot'],
-  }
-  
-  class { 'postfix::mastercf':
-    source => 'puppet:///modules/txtcmdr/master.cf',
-    require => Class['txtcmdr'],
-  }
-  ->
-  postfix::postconf{'virtual_transport':
-    value => 'dovecot',
-  }
- ->
-  postfix::postconf{'dovecot_destination_recipient_limit':
-    value => '1',
-  }
-*/
 }
